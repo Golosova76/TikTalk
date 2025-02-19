5d82de09978ab56f33161c04610d044a16f64ca7
Anna Golosova <a.golosova@3l.ru>
2025-02-19 17:52:48 +0300

    add service and pipe

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,11 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'tik-talk' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('tik-talk');
-  });
+
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
