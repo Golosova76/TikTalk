db4e1f116fb03c0959af3e0bfcbbf9e3aadec31c
Anna Golosova <golosova76@gmail.com>
2026-05-14 10:53:11 +0300

    feat: add block buttons in search page

--- src/app/common-ui/profile-card/profile-card.component.ts
+++ src/app/common-ui/profile-card/profile-card.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { Profile } from '../../data/interfaces/profile.interface';
 import { ImgUrlPipe } from '../../helpers/pipes/img-url.pipe';
+import { RouterLink } from '@angular/router';
+import { SvgIconComponent } from '../svg-icon/svg-icon.component';
 
 @Component({
   selector: 'app-profile-card',
-  imports: [ImgUrlPipe],
+  imports: [ImgUrlPipe, RouterLink, SvgIconComponent],
   templateUrl: './profile-card.component.html',
   styleUrl: './profile-card.component.scss',
 })
--- src/app/pages/search-page/search-page.component.ts
+++ src/app/pages/search-page/search-page.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, Renderer2 } from '@angular/core';
 import { ProfileCardComponent } from '../../common-ui/profile-card/profile-card.component';
 import { ProfileService } from '../../data/services/profile.service';
 import { ProfileFiltersComponent } from './profile-filters/profile-filters.component';
+import { debounceTime, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-search-page',
@@ -9,7 +10,29 @@ import { ProfileFiltersComponent } from './profile-filters/profile-filters.compo
   templateUrl: './search-page.component.html',
   styleUrl: './search-page.component.scss',
 })
-export class SearchPageComponent {
-  profileService = inject(ProfileService);
+export class SearchPageComponent implements AfterViewInit {
+  private readonly profileService = inject(ProfileService);
+  private readonly hostElement = inject(ElementRef);
+  private readonly r2 = inject(Renderer2);
+
   profiles = this.profileService.filteredProfiles;
+
+  ngAfterViewInit() {
+    this.resizeFeed();
+
+    fromEvent(window, 'resize')
+      .pipe(debounceTime(300))
+      .subscribe(() => {
+        this.resizeFeed();
+      });
+  }
+
+  resizeFeed() {
+    const feedElement = this.hostElement.nativeElement.querySelector('.scrollable-feed');
+    if (!feedElement) return;
+
+    const { top } = feedElement.getBoundingClientRect();
+    const height = window.innerHeight - top - 24;
+    this.r2.setStyle(feedElement, 'height', `${height}px`);
+  }
 }
